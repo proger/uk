d3c314b52dbaad8a740751248af20151e277313f
Volodymyr Kyrylov <vol@wilab.org.ua>
2022-04-20 00:25:33 +0300

    g2p: conflate some phones

--- uk/g2p.py
+++ uk/g2p.py
@@ -12,4 +12,12 @@ from ukro_g2p.predict import G2P
 
 __all__ = ['g2p']
 
-g2p = G2P('ukro-base-uncased')
+g2p_base = G2P('ukro-base-uncased')
+
+replacements = {
+    'SH23': 'SH2',
+    'H3': 'H',
+}
+
+def g2p(word):
+    return [replacements.get(p, p) for p in g2p_base(word)]
