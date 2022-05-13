3bae2803f7aae5e6a36f53c0b35b387f5330c25e
Volodymyr Kyrylov <vol@wilab.org.ua>
2022-05-14 01:17:25 +0300

    uk.prepare_lang: add format_ngram_lm

--- uk/prepare_lang.py
+++ uk/prepare_lang.py
@@ -33,14 +33,25 @@ def extend_dict(words: List[str], dict_dir: Path, source_dict_dir: Path, g2p_bat
             print(word, oov[word].strip(), file=f)
 
 
+def format_ngram_lm(corpus_txt: Path, work_dir: Path, lexiconp_txt: Path, order=2):
+    sh('ngram-count', '-text', corpus_txt,
+                      '-order', order, '-unk', '-lm',
+                       work_dir / "arpa")
+    sh('gzip', '-f', work_dir / 'arpa')
+    sh('utils/format_lm.sh', work_dir / 'lang', work_dir / 'arpa.gz', lexiconp_txt, work_dir / 'lang')
+
+
 if __name__ == '__main__':
+    from uk.dynamic import import_function
+
     parser = argparse.ArgumentParser(description="""\
-    Prepare a langdir and dictionary given a corpus
+    Prepare a langdir, dictionary and LM given a corpus
 
     python3 -m uk.prepare_lang -w exp/lang -o data/lang data/local/semesyuk_farshrutka/01_prologue.txt
     """, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o', '--output-dir', type=Path, required=True)
     parser.add_argument('-d', '--dict-dir', type=Path, default='data/local/dict', help='source dictionary')
+    parser.add_argument('--g2p_batch', type=import_function, default='uk.g2p:g2p_batch', help='batched g2p implementation')
     parser.add_argument('corpus_txt', type=Path, help='must be tokenized (see README)')
 
     args = parser.parse_args()
@@ -54,3 +65,4 @@ if __name__ == '__main__':
     extend_dict(words, dict_dir, args.dict_dir)
 
     sh('utils/prepare_lang.sh', dict_dir, "<unk>", args.output_dir / 'lang_tmp', langdir)
+    format_ngram_lm(args.corpus_txt, args.output_dir, args.output_dir / 'lang_tmp/lexiconp.txt', order=3)
